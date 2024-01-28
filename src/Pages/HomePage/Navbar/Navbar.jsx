3fda8f53e0e672afb0d21ae8c36a949870137ff7
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2024-01-28 11:51:01 +0600

    add basic layout for this proiject

--- src/Pages/HomePage/Navbar/Navbar.jsx
+++ src/Pages/HomePage/Navbar/Navbar.jsx
@@ -1,7 +1,24 @@
 const Navbar = () => {
     return (
         <div>
-this is navbar            
+
+            {/* navbar logo start */}
+            <div className="flex items-center my-2">
+                <img
+                    className="px-6 py-2 border-e-[1px] border-black"
+                    src="https://avatars.mds.yandex.net/get-bunker/128809/0efb578cd41a9939800311677e6f4f8a12f04e90/orig"
+                    alt="" />
+
+
+                <img
+                    className="h-[48px]"
+                    src="https://eda.yandex/images/3513074/25b26e65428af401c5d3651be009ac9f.png"
+                    alt="" />
+            </div>
+            {/* navbar logo end */}
+
+
+
         </div>
     );
 };
