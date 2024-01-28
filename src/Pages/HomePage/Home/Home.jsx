bfdf4af19172da6c74dbf7db750a0e4ce6d1db98
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2024-01-28 13:14:56 +0600

    add navbar section

--- src/Pages/HomePage/Home/Home.jsx
+++ src/Pages/HomePage/Home/Home.jsx
@@ -7,12 +7,15 @@ import SideBar from "../SideBar/SideBar";
 const Home = () => {
     return (
         <div className="bg-[#F5F4F2]">
+
             <Navbar />
+
             <div className="grid grid-cols-5 max-w-[1508px] mx-auto">
                 <SideBar />
                 <div className="col-span-3"><MainContent /></div>
                 <Cart />
             </div>
+
             <Footer />
         </div>
     );
--- src/Pages/HomePage/Navbar/Navbar.jsx
+++ src/Pages/HomePage/Navbar/Navbar.jsx
@@ -1,20 +1,19 @@
 import { FiSearch } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import { TbLocationFilled } from "react-icons/tb";
 
 const Navbar = () => {
     return (
-        <div className="grid grid-cols-12">
+        <div className="grid grid-cols-12 mx-6">
 
             {/* navbar logo start */}
             <div className="col-span-3 flex items-center my-4">
                 <Link to={"/"}>
                     <img
-                        className="px-5 py-2 border-e-[1px] border-black"
+                        className="pe-5 py-2 border-e-[1px] border-black"
                         src="https://avatars.mds.yandex.net/get-bunker/128809/0efb578cd41a9939800311677e6f4f8a12f04e90/orig"
                         alt="" />
                 </Link>
-
-
                 <img
                     className="h-[48px] px-5"
                     src="https://eda.yandex/images/3513074/25b26e65428af401c5d3651be009ac9f.png"
@@ -24,8 +23,7 @@ const Navbar = () => {
 
 
             {/* input section start */}
-            <form
-                className="flex items-center col-span-3 ms-10 w-[80%]">
+            <form className="col-span-3 flex items-center ms-10 w-[80%]">
 
                 <div className="grid grid-cols-12 rounded-2xl border-gray-400 border-[1px] bg-[#FFFFFF] w-full">
 
@@ -39,6 +37,28 @@ const Navbar = () => {
             {/* input section end */}
 
 
+            {/* location section start */}
+            <div className="col-span-2 flex items-center">
+                <div className="flex items-center border-[1px]  border-gray-400 w-fit text-[#21201F] font-medium rounded-2xl pe-3">
+                    <div className="ps-4 pr-2 py-3">
+                        <TbLocationFilled className="text-xl" />
+                    </div>
+                    Enter delivery address
+                </div>
+            </div>
+            {/* location section end */}
+
+
+            {/* login btn start */}
+            <div className="col-span-4 flex items-center justify-end">
+                <button className="bg-[#E7E6E4] font-medium text-base px-6 py-3 rounded-2xl flex items-center justify-center">
+                    Log in
+                </button>
+            </div>
+            {/* login btn end */}
+
+            {/* hr line */}
+            <hr className="my-1 border-[1px] border-gray-300 col-span-12" />
         </div>
     );
 };
